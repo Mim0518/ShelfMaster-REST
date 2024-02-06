fd107b5bb359183d4a55075c89169dd38d7be788
Guillermo Moreno <morenorivera1234@gmail.com>
2024-02-06 14:50:11 -0600

    Added boilerplate to init the ShelfController

--- src/main/java/com/afdevelopment/biblioteca/controller/ShelfController.java
+++ src/main/java/com/afdevelopment/biblioteca/controller/ShelfController.java
@@ -1,4 +1,15 @@
 package com.afdevelopment.biblioteca.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RequestMapping("/shelf")
+@RestController
 public class ShelfController {
+    private final String OPCORRECTA = "Operación correcta";
+    private final String DETAIL = "detailResponse";
+    private final String SUCCESSCODE = "SUC-003";
+    private final String RESPONSEDETAIL = "shelfDetail";
+    private static final Logger logger = LoggerFactory.getLogger(ShelfController.class);
 }
