2528960e31cd498f5096f430eef769c353a23f7c
Guillermo Moreno <morenorivera1234@gmail.com>
2024-07-06 15:06:40 -0600

    Librarian Classes created

---
+++ src/main/java/com/afdevelopment/biblioteca/model/Librarian.java
@@ -0,0 +1,85 @@
+package com.afdevelopment.biblioteca.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+public class Librarian {
+    @jakarta.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer Id;
+    private String username;
+    private String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private String password;
+    private String permisos;
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPermisos() {
+        return permisos;
+    }
+
+    public void setPermisos(String permisos) {
+        this.permisos = permisos;
+    }
+
+    @Override
+    public String toString() {
+        return "Librarian{" +
+                "Id=" + Id +
+                ", email='" + username + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", apellidoPaterno='" + apellidoPaterno + '\'' +
+                ", apellidoMaterno='" + apellidoMaterno + '\'' +
+                ", password='" + password + '\'' +
+                ", permisos='" + permisos + '\'' +
+                '}';
+    }
+}
